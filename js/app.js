16b825a322680f0f90f85e0a9173326d4f34b7f9
Justin Stowe <justinstowe12@gmail.com>
2023-05-19 19:38:57 -0600

    set up basic HTML of project.

---
+++ js/app.js
@@ -0,0 +1,16 @@
+class Hero {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
+}
+
+class Villain {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
+}
+
+const hero = new Hero("Multiplication Mastermind");
+const villain = new Villain("The Calculator");
